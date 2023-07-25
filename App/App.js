bc253a813d4a01c06b078b88aeed3aeb7ed1a4e8
Harry <yashaswikhurana.bt22cseds@pec.edu.in>
2023-07-25 08:07:28 +0530

    Update App.js

--- App/App.js
+++ App/App.js
@@ -7,8 +7,53 @@ export default function App() {
 
 
     return (
-      <View style={StyleSheet.container}>
-        <Text>name:{name},age:{age}</Text>
+      <View style={styles.container}>
+
+        <TextInput style={styles.input}
+        placeholder='Enter Name'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Student id'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Branch'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Room number'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Mobile number'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Pec mail-id'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.reason}
+        placeholder='Reason'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Address of visiting place'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Parents Mobile number'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='Date'
+        onChangeText={(val)=>setName(val)}/>
+
+        <TextInput style={styles.input}
+        placeholder='OTP'
+        onChangeText={(val)=>setName(val)}/>
+
+        {/* <Text>name:{name},age:{age}</Text> */}
       </View>
     );
   }
@@ -16,8 +61,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F2EAD3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200,
+    borderRadius:10,
+  reason : {
+    borderWidth:1,
+    borderColor:'#777',
+    padding:50,
+    margin:10,
+    width:200,
+    borderRadius:10,
+  }
+  }
 })
