62b7de9eb345ec096ec72daf98054b88632ed8ff
Harry <yashaswikhurana.bt22cseds@pec.edu.in>
2023-07-26 13:31:44 +0530

    Update App.js

--- App/App.js
+++ App/App.js
@@ -1,60 +1,80 @@
-import { Text, View, StyleSheet, TextInput } from 'react-native'
+import { Text, View, StyleSheet, TextInput,ScrollView, SafeAreaView,StatusBar,Button,Alert, } from 'react-native'
 import React, { useState } from 'react'
 
 export default function App() {
   const [name,setName] = useState('Yashaswi');
-  const [age,setAge] = useState('20');
+  const [sid,setsid] = useState('22106023');
+  const [branch,setbranch] = useState('Data-Science');
+  const [room_number,setrn] = useState('21');
+  const [mobile_number,setmn] = useState('7719417331');
+  const [Pec_mail_id,setpmi] = useState('yashaswikhurana.bt22cseds@pec.edu.in');
+  const [Reason,setreason] = useState('Going home');
+  const [AVP,setavp] = useState('AVP');
+  const [PMN,setpmn] = useState('9815494849');
+  const [Date,setdate] = useState('dd/mm/yyyy');
 
 
     return (
-      <View style={styles.container}>
-
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.ScrollView}>
         <TextInput style={styles.input}
         placeholder='Enter Name'
         onChangeText={(val)=>setName(val)}/>
 
-        <TextInput style={styles.input}
+        <TextInput style={styles.sid}
+        keyboardType='numeric'
         placeholder='Student id'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val2)=>setsid(val2)}/>
 
         <TextInput style={styles.input}
         placeholder='Branch'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val3)=>setbranch(val3)}/>
 
         <TextInput style={styles.input}
         placeholder='Room number'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val4)=>setrn(val4)}/>
 
         <TextInput style={styles.input}
         placeholder='Mobile number'
-        onChangeText={(val)=>setName(val)}/>
+        keyboardType='numeric'
+        onChangeText={(val5)=>setmn(val5)}/>
 
         <TextInput style={styles.input}
         placeholder='Pec mail-id'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val6)=>setpmi(val6)}/>
 
         <TextInput style={styles.reason}
         placeholder='Reason'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val7)=>setreason(val7)}/>
 
         <TextInput style={styles.input}
         placeholder='Address of visiting place'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val8)=>setavp(val8)}/>
 
         <TextInput style={styles.input}
         placeholder='Parents Mobile number'
-        onChangeText={(val)=>setName(val)}/>
+        keyboardType='numeric'
+        onChangeText={(val9)=>setpmn(val9)}/>
 
         <TextInput style={styles.input}
         placeholder='Date'
-        onChangeText={(val)=>setName(val)}/>
+        onChangeText={(val10)=>setdate(val10)}/>
 
         <TextInput style={styles.input}
         placeholder='OTP'
         onChangeText={(val)=>setName(val)}/>
+        <View style={styles.fixToText}>
+          <Button
+          title="Submit"
+          onPress={() => Alert.alert('Your form has been submitted and is under process')}
+        /></View>
+        
+        </ScrollView>
+
+        
 
         {/* <Text>name:{name},age:{age}</Text> */}
-      </View>
+      </SafeAreaView>
     );
   }
 
@@ -64,21 +84,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#F2EAD3',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+
+    // marginTop: 20,
   },
   input : {
     borderWidth:1,
     borderColor:'#777',
     padding:8,
     margin:10,
-    width:200,
-    borderRadius:10,
+    width:300,
+    borderRadius:10,},
   reason : {
     borderWidth:1,
     borderColor:'#777',
-    padding:50,
+    padding:8,
     margin:10,
-    width:200,
+    width:300,
     borderRadius:10,
+  },
+    sid : {
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:300,
+    borderRadius:10,
+    
+  },
+  fixToText:{
+    width: 90,
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft: 110,
   }
-  }
-})
+  },
+)
